a69dc9c4ffb57f662e98436ee167e2a4a0eac477
Ahmad Maulana Dani Akbar <akbar.maulanadani@gmail.com>
2024-02-26 21:44:59 +0700

    satu

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\POSController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/homee', 'App\Http\Controllers\HomeController@index');
+Route::get('/home',[HomeController::class,'home']);
 
 Route::prefix('products')->group(function () {
-    Route::get('/', 'ProductController@index');
-    Route::get('/category/food-beverage', 'ProductController@foodBeverage');
-    Route::get('/category/beauty-health', 'ProductController@beautyHealth');
-    Route::get('/category/home-care', 'ProductController@homeCare');
-    Route::get('/category/baby-kid', 'ProductController@babyKid');
+    Route::get('/products', [ProductController::class,'products']);
+    Route::get('/category/food-beverage', [ProductController::class,'food_beverage']);
+    Route::get('/category/beauty-health', [ProductController::class,'beauty_health']);
+    Route::get('/category/home-care', [ProductController::class,'home_care']);
+    Route::get('/category/baby-kid', [ProductController::class,'baby_kid']);
 });
 
-Route::get('/user/{id}/name/{name}', 'UserController@show');
-Route::get('/pos', 'POSController@index');
+Route::get('/user/{id}/name/{name}', [UserController::class,'user']);
+Route::get('/pos', [POSController::class,'pos']);
 
    
